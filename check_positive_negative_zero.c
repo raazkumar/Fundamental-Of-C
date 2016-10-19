cae4c70abf437827c5b077a4dc476ede8a30443d
Raj Kumar Nepal <admin@literaturepark.com>
2016-10-19 11:45:19 +0545

    Update check_positive_negative_zero.c

--- check_positive_negative_zero.c
+++ check_positive_negative_zero.c
@@ -1,6 +1,5 @@
-//Students Name: Raj Kumar Nepal
-//Subject: Programming Fundamentals
-//Roll No:
+//Author: Raj Kumar Nepal
+//Subject: Fundamentals of C Programming
 //Program: Program to check whether the number is zero or neg or positive.
 //Lab: 04
 //Date: 2016/09/27
@@ -9,16 +8,23 @@
 void main()
 {
     float a;
+    
     printf("Enter the  number to check: ");
     scanf("%f", &a);
+    
+    //Checking zero
     if(a=0){
         printf("The number is zero.");
     }
+    //Checking negative
     else if(a<0){
         printf("The number is negative.");
     }
+    //Else positive
     else{
         printf("The number is positive.");
     }
+    
     getch();
+    
 }
