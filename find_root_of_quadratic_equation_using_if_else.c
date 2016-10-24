311ef4931a9ddafcbf5ff133cb14031a9cdb3c36
Raj Kumar Nepal <admin@literaturepark.com>
2016-10-24 11:59:26 +0545

    Update find_root_of_quadratic_equation_using_if_else.c

--- find_root_of_quadratic_equation_using_if_else.c
+++ find_root_of_quadratic_equation_using_if_else.c
@@ -1,6 +1,5 @@
-//Student Name: Raj Kumar Nepal
-//Roll No: 
-//Subject: Fundamental of Programming
+//Author: Raj Kumar Nepal
+//Subject: Fundamental of C Programming
 //Program: Write a program to take two input from user and print largest and smallest number using ternary operator....
 //Date: 24 Oct, 2016
 //root1 = -d+sqrt((b^2-4ac))
@@ -14,24 +13,23 @@ int main()
 	
 	printf("Enter Co-efficient of a :");
 	scanf("%f",&a);
-
 	printf("Enter Co-efficient of b :");
 	scanf("%f",&b);
-
 	printf("Enter Co-efficient of c :");
 	scanf("%f",&c);
 
 	r=(pow(b,2)-4*a*c);
+	
 	if (d>=0){
 		
 	d=sqrt(r);
 
 	r1=(-b+d)/(2*a);
 	r2=(-b-d)/(2*a);
-
 	printf("Root One : %f \n",r1);
 	printf("Root Two : %f",r2);
 	}
+	
 	else{
 	printf("Root is imaginary");	
 	}
