91fe301de0dc76809400caf99eabeb713456b6aa
Raj Kumar Nepal <admin@literaturepark.com>
2016-10-24 11:18:36 +0545

    Update largest_of_two_number_using_ternary_operator.c
    Program to find largest number of two numbers using ternary operator

--- largest_of_two_number_using_ternary_operator.c
+++ largest_of_two_number_using_ternary_operator.c
@@ -1,19 +1,12 @@
-//Student Name: Raj Kumar Nepal
-//Roll No: 
-//Subject: Fundamental of Programming
+//Athor: Raj Kumar Nepal 
+//Subject: Fundamental of C Programming
 //Program: Write a program to take two input from user and print largest and smallest number using ternary operator....
 //Date: 24 Oct, 2016
-
-//Ternary operator 
-//takes 3 arguements
-//syntax
-//(condition) ? True : False
-
-
 #include<stdio.h>
 int main()
 {
 	int a,b,c,d;
+	
 	printf("Enter first number: ");
 	scanf("%d",&a);
 	printf("Enter second number: ");
@@ -21,9 +14,27 @@ int main()
 	
 	c=(a>b) ? a : b;
 	d=(a<b) ? a : b;
+	
 	printf("%d is largest number.\n",c);
 	printf("%d is smallest number.",d);
 	
+return 0;
+}
+
+
+//Shortcut method...
+
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	
+	printf("Enter first number: ");
+	scanf("%d",&a);
+	printf("Enter second number: ");
+	scanf("%d",&b);
+	
+	(a>b) ? printf("%d is largest number.\n",a) : printf("%d is smallest number.",a) ;
 
 return 0;
 }
