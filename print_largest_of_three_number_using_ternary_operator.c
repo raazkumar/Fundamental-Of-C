8b5c67b903920c4ff5d69ec42524ec76b0ef320e
Raj Kumar Nepal <admin@literaturepark.com>
2016-10-24 11:28:32 +0545

    Update print_largest_of_three_number_using_ternary_operator.c

--- print_largest_of_three_number_using_ternary_operator.c
+++ print_largest_of_three_number_using_ternary_operator.c
@@ -1,12 +1,12 @@
-//Student Name: Raj Kumar Nepal
-//Roll No: 
-//Subject: Fundamental of Programming
+//Author: Raj Kumar Nepal
+//Subject: Fundamental of C Programming
 //Program: Write a program to print greatest among three numbers using ternary operator...
 //Date: 24 Oct, 2016
 
 #include<stdio.h>
 int main(){
 	int a,b,c;
+	
 	printf("Enter first number: ");
 	scanf("%d",&a);
 	printf("Enter second number: ");
@@ -15,10 +15,27 @@ int main(){
 	scanf("%d",&c);
 	
 	(a>=b && a>=c) ? printf("%d is largest.",a) : (b>=a && b>=c) ? printf("%d is largest.",b) : printf("%d is largest.",c) ;
-	
-	(a>=b) ? (a>=c ? printf("%d is largest.",a) : printf("%d is largest.",c) ) : (b>=c ? printf("%d is largest.",b) : printf("%d is largest",c));   
-	
-	
-	
+
 return 0;	
 }
+
+
+//Method 2:
+
+
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	
+	printf("Enter first number: ");
+	scanf("%d",&a);
+	printf("Enter second number: ");
+	scanf("%d",&b);
+	printf("Enter third number: ");
+	scanf("%d",&c);
+	
+(a>=b) ? (a>=c ? printf("%d is largest.",a) : printf("%d is largest.",c) ) : (b>=c ? printf("%d is largest.",b) : printf("%d is largest",c));   
+
+return 0;	
+}
+
