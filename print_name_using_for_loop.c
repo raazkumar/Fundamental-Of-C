fc820ed96e1e8063f4bb46d5e2fcef047aea3a8c
Raj Kumar Nepal <admin@literaturepark.com>
2016-11-15 10:21:01 +0545

    Add files via upload
    Print name ten times using for loop

---
+++ print_name_using_for_loop.c
@@ -0,0 +1,18 @@
+/*
+Author: Raj Kumar Nepal
+Subject: Fundamentals of C Programming
+Program: C Program to print name ten times using for loop.
+Date: 2016/11/15
+*/
+
+#include<stdio.h>
+int main(){
+	int i;
+	
+	for (i=1; i<=10; i++)
+		{
+		printf("%d. My name is Raj Kumar Nepal.\n",i);
+		}
+		
+return 0;
+	}
